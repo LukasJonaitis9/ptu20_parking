b8851fd3519498a57e85f7bff81c396422f09837
Lukas Jonaitis <lukasjonaitisltu@gmail.com>
2024-02-01 14:43:46 +0200

    antras

--- Lgui_main_langai.py
+++ Lgui_main_langai.py
@@ -1,3 +1,5 @@
+import PySimpleGUI as sg
+
 ##tarifu listas(list /create/ update /delete)
 ##cars list(list/ create/ delete)
 ##arrival(choose/create car; create parking entry with  datetime.now() as arival time)
@@ -8,3 +10,25 @@
     ##find parking bills by specific cars
     ##total parking revenue by between dates with list
 ## main window
+main_layout = [
+    [
+        sg.Button('Tariffs,', key='-TARIFFS-', size = 10),
+        sg.Button('Cars,', key='-CARS-', size = 10),
+     ],
+     
+        [
+        sg.Button('Arrivals,', key='-ARRIVALS-', size = 10),
+        sg.Button('Departure,', key='-DEPARTURE-', size = 10),
+     ],
+
+         [
+        sg.Button('Reports,', key='-REPORTS-', size = 21),
+        
+     ],
+]
+main_window = sg.Window('Parking PTU20', main_layout, font = 'sans-serif 20', element_justification='center', size = 400, 300)
+
+while True:
+    event, vlues = main_window.read()
+    if event == sg.WIN_CLOSED:
+        break
